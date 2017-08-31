33d3c4da8eb15d216c50bf3bd9d9ed8bf1343a5d
paul9508 <paul9508@gmail.com>
2017-09-01 02:02:21 +0800

    Update login.php

--- function/login.php
+++ function/login.php
@@ -6,7 +6,7 @@ require_once('../class/User.php');
 $database = new Database();
 $db = $database->DbConnection();
 $user= new User($db);
-$username=$_POST['username'];
+$username=htmlspecialchars($_POST['username']);
 $password=hash('sha256', $_POST['password']);
 $user->setUsername($username);
 $sql="SELECT user.username ,user.password ,GROUP_CONCAT(user_roles.role SEPARATOR ' ') as role FROM user,user_roles WHERE user.username=user_roles.username and user.username=:username GROUP BY user.username";
